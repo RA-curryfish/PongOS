319036b14c9a9d86a18c75b0490561af2d6834ac
rahul <rahul.shevade@gmail.com>
2024-06-30 21:55:25 -0400

    adding cursor movements

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,5 @@
 #include "utils.h"
-#include "print.h"
+#include "terminal.h"
 #include "kbd.h"
 
 #ifdef __linux__
@@ -39,21 +39,18 @@ void init_pics(int pic1, int pic2)
 	outb(PIC1 + 1, 0xFF);
 }
 
-bool capslock = false;
+bool capslock = false; // todo: query for capslock state
 bool shift = false;
 
 void kernel_main(void) 
 {
 	/* Initialize terminal interface */
 	terminal_initialize();
-
-	// for (int8_t i = 0; i<VGA_HEIGHT-1;i++)
-	// 	for(int8_t j = 0; j<VGA_WIDTH;j++)
-	// 		printchar('a'+(i-'0'));
 	printstr("**********************************************\n");
 	printstr("\t\tPong OS\n");
 	printstr("**********************************************\n");
 	// init_pics(0x20, 0x28);
+	
 	uint8_t c = 0;
 	uint8_t s = 0;
 	uint16_t data_port = 0x60;
@@ -62,16 +59,16 @@ void kernel_main(void)
 		s = inb(status_port);
 		if (((s&1)==1)) {
 			c = inb(data_port);
-			if (c>=0x80) continue;
-			if (kbd[c] != -1 && kbd[c] != -2 && kbd[c] != -3) { // not backspace, end or capslock
-				if ((capslock && !shift) || (!capslock && shift)) { printchar(caps_kbd[c]); } // todo: fix shift
-				// if (kbd[c] >= 97 && kbd[c] <= 122 && shift) ((capslock && !shift) || (!capslock && shift))) {printchar(caps_kbd[c]); }
+			if (c>=0x80) { continue;}
+			else if (kbd[c] != -1 && kbd[c] != -2 && kbd[c] != -3) { // not backspace, end or capslock
+				if (kbd[c] >= 97 && kbd[c] <= 122 && (capslock || shift)) { printchar(caps_kbd[c]); }
 				else { printchar(kbd[c]); }
 			}
 			else if (kbd[c] == -1) backspace();
 			else if (kbd[c] == -3) capslock = !capslock;
-			// else if (kbd[c] == -4) shift = 1; // todo: fix shift
+			// else if (kbd[c] == -4) shift = true; // todo: fix shift
 			// else if (kbd[c] == -5) shift = 0;
+			else if (c == 0x2A || c == 0x36) shift = true;
 		}
 	}while(kbd[c] != -2);
 }
--- src/terminal.h
+++ src/terminal.h
@@ -4,14 +4,13 @@
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 
-void terminal_putchar(char c);
-void printchar(char c);
+// void terminal_putchar(char c);
+// void printchar(char c);
 
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
-uint16_t cursor;
 uint16_t* terminal_line_ptrs[VGA_HEIGHT];
 
 /* Hardware text mode color constants. */
@@ -49,6 +48,31 @@ void terminal_setcolor(uint8_t color)
 	terminal_color = color;
 }
 
+void disable_cursor()
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, 0x20);
+}
+
+void enable_cursor(uint8_t cursor_start, uint8_t cursor_end)
+{
+	outb(0x3D4, 0x0A);
+	outb(0x3D5, (inb(0x3D5) & 0xC0) | cursor_start);
+
+	outb(0x3D4, 0x0B);
+	outb(0x3D5, (inb(0x3D5) & 0xE0) | cursor_end);
+}
+
+void update_cursor(int x, int y)
+{
+	uint16_t pos = y * VGA_WIDTH + x;
+
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (uint8_t) (pos & 0xFF));
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+}
+
 void clear_line(size_t row)
 {
 	for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -67,21 +91,14 @@ void clear_terminal()
 	}
 }
 
-void terminal_putcharat(char c, uint8_t color, size_t x, size_t y) 
+void terminal_initialize(void) 
 {
-	const size_t index = y * VGA_WIDTH + x; // x is width, y is height
-	terminal_buffer[index] = vga_entry(c, color);
-}
-void printchar(char data) 
-{
-	terminal_putchar(data);
-}
-
-void printstr(const char *data)
-{
-	size_t size = strlen(data);
-	for (size_t i = 0; i < size; i++)
-		printchar(data[i]);
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) VGA_MEM_BASE;
+	clear_terminal();
+	for (size_t y = 0; y < VGA_HEIGHT; y++) terminal_line_ptrs[y] = (uint16_t*)(sizeof(uint16_t)*y*VGA_WIDTH+VGA_MEM_BASE);
 }
 
 void scroll_terminal()
@@ -95,6 +112,13 @@ void scroll_terminal()
 	}
 	clear_line(VGA_HEIGHT-1);
 }
+
+void terminal_putcharat(char c, uint8_t color, size_t x, size_t y) 
+{
+	const size_t index = y * VGA_WIDTH + x; // x is width, y is height
+	terminal_buffer[index] = vga_entry(c, color);
+}
+
 void terminal_putchar(char c) 
 {
 	switch(c) 
@@ -116,18 +140,21 @@ void terminal_putchar(char c)
 					terminal_row = 0;
 			}
 	}
+	update_cursor(terminal_column, terminal_row);
 	if (terminal_row == 0 && terminal_column == 0)
 		scroll_terminal();
 }
 
-void terminal_initialize(void) 
+void printchar(char data) 
 {
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) VGA_MEM_BASE;
-	clear_terminal();
-	for (size_t y = 0; y < VGA_HEIGHT; y++) terminal_line_ptrs[y] = (uint16_t*)(sizeof(uint16_t)*y*VGA_WIDTH+VGA_MEM_BASE);
+	terminal_putchar(data);
+}
+
+void printstr(const char *data)
+{
+	size_t size = strlen(data);
+	for (size_t i = 0; i < size; i++)
+		printchar(data[i]);
 }
 
 void backspace()
@@ -143,4 +170,5 @@ void backspace()
 		--terminal_column;
 	}
 	terminal_putcharat(' ',terminal_color,terminal_column,terminal_row);
+	update_cursor(terminal_column, terminal_row);
 }
