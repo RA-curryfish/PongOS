f0bf8135ce982f175bb81e427ee79d5dd1868ba3
rahul <rahul.shevade@gmail.com>
2024-07-15 19:29:44 -0400

    start context switching

--- src/kernel.c
+++ src/kernel.c
@@ -11,6 +11,28 @@
 #error "not using x86 compiler??"
 #endif
 
+bool pid_arr[8];
+uint8_t pid_ctr=1;
+
+typedef struct process_control_block {
+	uint8_t pid;
+	uint32_t stack_top;
+	uint16_t cr3;
+	struct process_control_block* next_task;
+	uint16_t state
+	// todo: add other fields
+}pcb;
+
+void create_pcb()
+{
+	
+}
+
+void switch_task(uint32_t addr)
+{
+	
+}
+
 void splash_screen()
 {
 	/* Initialize terminal interface */
@@ -58,6 +80,9 @@ void keyboard_handle()
 
 void kernel_main(void) 
 {
-	splash_screen();
-	keyboard_handle();
+	// create PCB for kernel with pid = 1
+	pid_arr[pid_ctr] = true;
+	create_pcb();
+	// splash_screen();
+	// keyboard_handle();
 }
